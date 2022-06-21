fffc4ffb5b9c8ec628cc705d604f1dca5e81543b
caozhongqiang <caozhongqiang@hzchangchong.com>
2022-06-21 18:36:10 +0800

    init

---
+++ example/example-site/src/main/java/com/evol/ExampleSiteApplication.java
@@ -0,0 +1,13 @@
+package com.evol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSiteApplication.class, args);
+    }
+
+}
