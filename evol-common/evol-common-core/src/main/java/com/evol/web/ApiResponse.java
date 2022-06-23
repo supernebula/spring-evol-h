8639f8fc2e9b18d934be2c6bfdbf11552ac0d407
supernebula <supernebula@126.com>
2022-06-24 00:10:52 +0800

    init

--- evol-common/evol-common-core/src/main/java/com/evol/web/ApiResponse.java
+++ evol-common/evol-common-core/src/main/java/com/evol/web/ApiResponse.java
@@ -1,11 +1,8 @@
 package com.evol.web;
 
-
 import com.evol.enums.ApiResponseEnum;
 import lombok.Data;
-
 import java.io.Serializable;
-
 
 @Data
 public class ApiResponse<T> implements Serializable {
